d62b70e4a5489843a7910d7f893c6d903535a816
instrutorvilson <instrutorvilsonmoro@gmail.com>
2025-11-22 16:19:49 -0300

    Criando formulário de produtos com two-way-data-bind

--- src/app/produto/produto.ts
+++ src/app/produto/produto.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface IProduto {
+  id?:number,
+  descricao: string,
+  unMedida: string,
+  preco: number,
+  estoque: number
+}
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
+  produto:IProduto = {descricao:'',preco:0, unMedida: 'LT',estoque:0}
 
+  salvar():void{
+    console.log(this.produto)
+  }
 }
